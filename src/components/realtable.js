9be32072daecc56b569f643a07d83db576e0bf6c
kunal-bhadra <rick101001@gmail.com>
2022-03-27 16:35:00 +0530

    working table & pagination, no colour

--- src/components/realtable.js
+++ src/components/realtable.js
@@ -1,27 +1,69 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-
-import { GridColDef } from '@mui/x-data-grid';
+import Pagination from '@mui/material/Pagination';
 import { createStyles, makeStyles } from '@mui/material';
 
 
+const datagridSx = {
+  '& .MuiDataGrid-columnHeaderTitle': {
+    textOverflow: "clip",
+    whiteSpace: "break-spaces",
+    lineHeight: 1, 
+  },
+  "& .MuiDataGrid-columnHeaders": {
+    backgroundColor: "rgba(39,61,74,255)",
+    color: "rgb(218,225,227)",
+    fontSize: 13
+  },
+  "& .MuiDataGrid-checkboxInput": {
+    color: "rgb(218,225,227)",
+  },
+  "& .MuiDataGrid-virtualScrollerRenderZone": {
+    "& .MuiDataGrid-row":{ 
+      backgroundColor: "rgba(44,66,80,255)" ,
+      color: "rgb(218,225,227)",
+      fontSize: 12
+    }
+  },
+  "& .MuiDataGrid-columnSeparator": {
+    visibility: 'hidden'
+  },
+  "& .MuiDataGrid-footerContainer": {
+    backgroundColor: "rgba(39,61,74,255)",
+  },
+  "& .Mui-selected": {
+    color: "rgb(218,225,227)",
+  }
+};
+
+
+function CustomPagination() {
+  return (
+    <Pagination
+      color="primary"
+    />
+  );
+}
+
+
+
 const columns = [
-    { field: 'id', headerName: 'Sl No', width: 60, headerAlign: 'center' },
-    { field: 'business_code', headerName: 'Business Code', width: 120, headerAlign: 'center' },
-    { field: 'cust_number', headerName: 'Customer Number', width: 120, headerAlign: 'center' },
-    { field: 'clear_date', headerName: 'Clear Date', width: 120, headerAlign: 'center' },
-    { field: 'business_year', headerName: 'Business Year', width: 120, headerAlign: 'center' },
-    { field: 'doc_id', headerName: 'Document ID', width: 120, headerAlign: 'center' },
-    { field: 'posting_date', headerName: 'Posting Date', width: 120, headerAlign: 'center' },
-    { field: 'document_create_date', headerName: 'Document Create Date', width: 120, headerAlign: 'center' },
-    { field: 'due_in_date', headerName: 'Due Date', width: 120, headerAlign: 'center' },
-    { field: 'invoice_currency', headerName: 'Invoice Currency', width: 120, headerAlign: 'center' },
-    { field: 'document_type', headerName: 'Document Type', width: 120, headerAlign: 'center' },
-    { field: 'posting_id', headerName: 'Posting ID', width: 120, headerAlign: 'center' },
-    { field: 'total_open_amount', headerName: 'Total Open Amount', width: 120, headerAlign: 'center' },
-    { field: 'baseline_create_date', headerName: 'Baseline Create Date', width: 120, headerAlign: 'center' },
-    { field: 'cust_payment_terms', headerName: 'Customer Payment Terms', width: 120, headerAlign: 'center' },
-    { field: 'invoice_id', headerName: 'Invoice ID', width: 120, headerAlign: 'center' },
+    { field: 'id', headerName: 'Sl No', width: 45, align: "center", headerAlign: 'center' },
+    { field: 'business_code', headerName: 'Business Code', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'cust_number', headerName: 'Customer Number', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'clear_date', headerName: 'Clear Date', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'business_year', headerName: 'Business Year', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'doc_id', headerName: 'Document ID', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'posting_date', headerName: 'Posting Date', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'document_create_date', headerName: 'Document Create Date', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'due_in_date', headerName: 'Due Date', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'invoice_currency', headerName: 'Invoice Currency', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'document_type', headerName: 'Document Type', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'posting_id', headerName: 'Posting ID', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'total_open_amount', headerName: 'Total Open Amount', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'baseline_create_date', headerName: 'Baseline Create Date', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'cust_payment_terms', headerName: 'Customer Payment Terms', width: 90, align: "center", headerAlign: 'center' },
+    { field: 'invoice_id', headerName: 'Invoice ID', width: 90, align: "center", headerAlign: 'center' },
 ];
 
 
@@ -75,26 +117,26 @@ const rows = [
 
 
 export default function DataTable() {
+  const [pageSize, setPageSize] = React.useState(10);
+
     return (
       <div style={{ width: '100%' }}>
         <DataGrid
             rows={rows} 
             columns={columns}
-            pageSize={10}
             checkboxSelection={true}
             autoHeight={true}
-            autoPageSize={true}
             density='compact'
             rowHeight='40'
             headerHeight={80}
             disableColumnMenu={true}
             disableSelectionOnClick={true}
-            sx={{
-            '& .MuiDataGrid-columnHeaderTitle': {
-                textOverflow: "clip",
-                whiteSpace: "break-spaces",
-                lineHeight: 1
-            }}}
+            sx={datagridSx}
+            
+            pageSize={pageSize}
+            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+            rowsPerPageOptions={[5, 10, 15]}
+            pagination
         />
       </div>
     );
